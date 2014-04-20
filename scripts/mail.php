a11886108069a65b847649309b45c2937da92ca2
Ryan <ryan.geo@gmail.com>
2014-04-20 11:30:58 -0700

    Update contact page

--- scripts/mail.php
+++ scripts/mail.php
@@ -2,19 +2,11 @@
 
     $name = $_POST['name'];
     $email = $_POST['email'];
-	$company = $_POST['company'];
-	$location = $_POST['location'];
-	$phone = $_POST['phone'];
-	$website = $_POST['website'];
-    $subject = $_POST['project'];
-	$timeline = $_POST['timeline'];
-	$budget = $_POST['budget'];
     $details = $_POST['details'];
-	$refer = $_POST['refer'];
     $from = $_POST['email'];
     $to = 'michelledavella@gmail.com';
     
-    $body = "From: $name\nEmail: $email\nCompany: $company\nLocation: $location\nPhone: $phone\nWebsite: $website\nProject: $subject\nTimeline: $timeline\nBudget: $budget\nReferred by: $refer\nDetails: $details";
+    $body = "From: $name\nEmail: $email\nDetails: $details";
     
     if ($_POST['submit']) {
         if ($name != '' && $email != '') {				 
